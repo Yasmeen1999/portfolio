e1524414dfb55c84dea11015fb196529a8a2552d
yasmeen1999 <yasmeenaleemkhan786@gmail.com>
2021-07-07 15:20:38 +0530

    modification

--- src/Home.js
+++ src/Home.js
@@ -26,6 +26,7 @@ function Home() {
                   }}>
                   Resume
                 </Link> */}
+                <Link to="/"> Resume</Link>
               </li>
             </ul>
           </nav>
